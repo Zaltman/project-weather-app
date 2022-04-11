70d7c7c8388642dbbf9cf64c49a777d4cfb1e23d
Marius <mariusdruskinis@gmail.com>
2022-04-11 18:31:32 +0300

    Interface improvements, weather info update on click(inwork)

--- script.js
+++ script.js
@@ -27,12 +27,16 @@ async function getWeatherData(lat, lon) {
   let response = await fetch(fetchStr, {
     mode: 'cors',
   });
+  // http://openweathermap.org/img/wn/10d@2x.png
   data = await response.json();
+  console.log(data);
+  console.log(data.weather[0].icon);
   domWeatherCard(
     data.name,
     data.weather[0].description,
     data.main.temp,
-    data.main.feels_like
+    data.main.feels_like,
+    data.weather[0].icon
   );
   console.log(
     'Weather in ' +
@@ -49,48 +53,61 @@ async function mainProcess() {
   let geoLoc = await getGeoLoc();
   getWeatherData(geoLoc[0].lat, geoLoc[0].lon);
 }
-mainProcess();
 
 function domWeatherCard(geoNameText, descriptionText, tempText, feelsLikeText) {
+  //check if weather card already exists and delete it if so
+  if (document.querySelector('.weatherCard')) {
+    document.querySelector('.weatherCard').remove();
+  }
+  let locationInputBtn = document.querySelector('#locSearchBtn');
   let cardsContainer = document.querySelector('.weatherCardsContainer');
   let weatherCard = document.createElement('div');
-  weatherCard.classList.add('weatherCard');
-
+  let geoNameContainer = document.createElement('div');
   let geoName = document.createElement('div');
+  let description = document.createElement('div');
+  let tempContainer = document.createElement('div');
+  let leftSide = document.createElement('div');
+  let rightSide = document.createElement('div');
+  let weatherIcon = document.createElement('img');
+  let temp = document.createElement('div');
+  let feelsLikeTemp = document.createElement('div');
+
+  locationInputBtn.addEventListener('click', locationBtnClick);
+
+  function locationBtnClick() {
+    let inputValue = document.querySelector('#locInput').value;
+    console.log(inputValue);
+    mainProcess();
+  }
+  weatherCard.classList.add('weatherCard');
+  geoNameContainer.classList.add('geoNameContainer');
   geoName.classList.add('geoName');
   geoName.textContent = geoNameText;
-
-  let description = document.createElement('div');
   description.classList.add('description');
   descriptionText =
     descriptionText.charAt(0).toUpperCase() + descriptionText.slice(1);
   description.textContent = descriptionText;
-
-  let tempContainer = document.createElement('div');
   tempContainer.classList.add('tempContainer');
-
-  let leftSide = document.createElement('div');
-  let rightSide = document.createElement('div');
   rightSide.classList.add('rightSide');
-
-  let temp = document.createElement('div');
+  weatherIcon.src =
+    'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
   temp.classList.add('temp');
-  temp.textContent = tempText + '°';
-
-  let feelsLikeTemp = document.createElement('div');
+  temp.textContent = tempText.toFixed(1) + '°';
   feelsLikeTemp.classList.add('feelsLike');
-  feelsLikeTemp.textContent = 'Feels like ' + feelsLikeText + '°';
+  feelsLikeTemp.textContent = 'Feels like ' + feelsLikeText.toFixed(1) + '°';
 
   cardsContainer.appendChild(weatherCard);
-  weatherCard.appendChild(geoName);
+  weatherCard.appendChild(geoNameContainer);
+  geoNameContainer.appendChild(geoName);
+  geoNameContainer.appendChild(weatherIcon);
   weatherCard.appendChild(tempContainer);
   tempContainer.appendChild(leftSide);
   tempContainer.appendChild(rightSide);
-
   leftSide.appendChild(temp);
-  rightSide.appendChild(feelsLikeTemp);
   rightSide.appendChild(description);
+  rightSide.appendChild(feelsLikeTemp);
 }
+mainProcess();
 
 // getWeatherData(55.8900579, 21.2424112);
 
